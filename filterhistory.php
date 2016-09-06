39826c0c65050b0c5456604c7ddd7ff86d49d07c
redhaanggara <redhabayuanggara@yahoo.co.id>
2016-09-06 18:10:50 +0700

    Update filterhistory.php

--- filterhistory.php
+++ filterhistory.php
@@ -23,7 +23,7 @@ $link = new mysqli("us-cdbr-azure-west-b.cleardb.com", "be826d4ad86399", "8670b0
 
 $postdata = file_get_contents('php://input');
 $request = json_decode($postdata);
-$uname = 'redha';
+$uname = mysql_real_escape_string($request->uname);
 
 $result = $link->query("SELECT A.id,A.tanggal,A.photo1,A.deskripsi,A.pengirim,A.location,A.lat,A.lng,A.like,B.dp from timelines A ,users B where A.pengirim ='$uname'  AND B.email = '$uname' ORDER BY A.id DESC");
 
