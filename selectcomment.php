8e63a7a0bdee5ce2efe34ffe98315918e5b5d184
redhaanggara <redhabayuanggara@yahoo.co.id>
2016-09-07 19:51:24 +0700

    Update selectcomment.php

--- selectcomment.php
+++ selectcomment.php
@@ -1,29 +1,9 @@
 <?php
-
-if (isset($_SERVER['HTTP_ORIGIN'])) {
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-    header('Access-Control-Allow-Credentials: true');
-    header('Access-Control-Max-Age: 86400');    // cache for 1 day
-}
-
-// Access-Control headers are received during OPTIONS requests
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-        header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-
-    exit(0);
-}
-
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
 
 $link = new mysqli("us-cdbr-azure-west-b.cleardb.com", "be826d4ad86399", "8670b078", "dbpalhub");
-
-$postdata = file_get_contents('php://input');
-$request = json_decode($postdata);
-$id = mysql_real_escape_string($request->id);
+$id = $_GET['idx'];
 
 $result = $link->query("SELECT A.time, A.comment, A.id, A.pengirim, B.dp FROM comment A, users B WHERE A.id = '$id' AND A.pengirim = B.email");
 
@@ -42,8 +22,8 @@ while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
     $outp .= '"dp":"'                       . $rs["dp"]                       . '"}';
 
 }
+
 $outp ='{"records":['.$outp.']}';
-$link->close();
 echo($outp);
 
 ?>
