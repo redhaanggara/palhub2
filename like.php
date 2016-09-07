9649dd986ee75a290002e491e6d4423d51d54866
redhaanggara <redhabayuanggara@yahoo.co.id>
2016-09-07 20:45:13 +0700

    Update like.php

--- like.php
+++ like.php
@@ -1,27 +1,11 @@
 <?php
 //Untuk antisipasi error No Access Control Allowed antar host
- if (isset($_SERVER['HTTP_ORIGIN'])) {
-        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-        header('Access-Control-Allow-Credentials: true');
-        header('Access-Control-Max-Age: 86400');    // cache for 1 day
-    }
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json; charset=UTF-8');
+header("Access-Control-Allow-Headers:origin,X-Request-With,Content-Type,Accept");
  
-    // Access-Control headers are received during OPTIONS requests
-    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
- 
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
- 
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-            header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
- 
-        exit(0);
-    }
- 
-
 // Create connection
 $mysqli=new mysqli("us-cdbr-azure-west-b.cleardb.com","be826d4ad86399","8670b078", "dbpalhub");
-
 
 $postdata = file_get_contents('php://input');
 $request = json_decode($postdata);
@@ -34,24 +18,21 @@ $resulttt = mysqli_query($mysqli,$sqlll);
 
   $count = mysqli_num_rows($resulttt); //Kalo data User yang dicari oleh query $sql ada ditabase, count akan bernilai 1
   if($count <= 0){
-     $sqll = "INSERT INTO userlike (idposting,likers) VALUES
-        ('$idx','$uname')";
-    mysqli_query($mysqli,$sqll);
-          
-    $result = "SELECT `like` From `timelines` WHERE id ='$idx'";
+     $sqll = "INSERT INTO userlike (idposting,likers) VALUES ('$idx','$uname')";
+     mysqli_query($mysqli,$sqll);
+     $result = "SELECT `like` From `timelines` WHERE id ='$idx'";
     
-    if($hasil = mysqli_query($mysqli,$result)){
-    while ($execute=mysqli_fetch_assoc($hasil)) {
-        $bs=$execute['like'];
-        }
-    }
+        if($hasil = mysqli_query($mysqli,$result)){
+        while ($execute=mysqli_fetch_assoc($hasil)) {
+               $bs=$execute['like'];
+           }
+         }
 
     $num=$bs=$bs+1;
     $sql = "UPDATE `timelines` SET `like` = $num where id = '$idx'";       
     mysqli_query($mysqli,$sql);
-  }
+   }
   else{
    echo "falseja";
 }
-exit();
 ?>
