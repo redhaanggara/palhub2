49dd39b6aa540350abafce4efa4a499942a3a84a
redhaanggara <redhabayuanggara@yahoo.co.id>
2016-09-07 16:56:31 +0700

    Update filterhistory.php

--- filterhistory.php
+++ filterhistory.php
@@ -24,34 +24,9 @@ header("Content-Type: application/json; charset=UTF-8");
 $link = new mysqli("localhost", "root", "", "jokedb");
 
 $uname = $_GET['person'];
-
-$result = $link->query("SELECT A.id,A.tanggal,A.photo1,A.deskripsi,A.pengirim,A.location,A.lat,A.lng,A.like,B.dp from timelines A ,users B where A.pengirim ='$uname'  AND B.email = '$uname' ORDER BY A.id DESC");
-
-
-$outp = "";
-while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
-    if ($outp != "") {$outp .= ",";}
-    $outp .= '{"id":"'                       . $rs["id"]                        . '",';
-
-    $outp .= '"tanggal":"'                       . $rs["tanggal"]                        . '",';
-
-    $outp .= '"photo1":"'                       . $rs["photo1"]                       . '",';
-
-    $outp .= '"deskripsi":"'                       . $rs["deskripsi"]                       . '",';
-
-    $outp .= '"pengirim":"'                       . $rs["pengirim"]                       . '",';
-
-    $outp .= '"location":"'                       . $rs["location"]                       . '",';
-
-    $outp .= '"lat":"'                       . $rs["lat"]                       . '",';
-
-    $outp .= '"lng":"'                       . $rs["lng"]                       . '",';
-
-    $outp .= '"like":"'                       . $rs["like"]                       . '",';
-
-    $outp .= '"dp":"'                       . $rs["dp"]                       . '"}';
-}
-$outp ='{"records":['.$outp.']}';
-echo($outp);
-echo "kampang";
+$sql = "SELECT A.id,A.tanggal,A.photo1,A.deskripsi,A.pengirim,A.location,A.lat,A.lng,A.like,B.dp from timelines A ,users B where A.pengirim ='$uname'  AND B.email = '$uname' ORDER BY A.id DESC";
+$result = mysqli_query($link,$sql);
+$data = mysqli_fetch_array($result, MYSQL_ASSOC); //Data user difetch dalam bentuk array lalu diubah jadi JSON biar bisa diolah per objek/fieldnya
+echo json_encode($data);
+exit();
 ?>
