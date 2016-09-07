a8cf53bdb43908d8c82ce3253be0897461f1ae6a
redhaanggara <redhabayuanggara@yahoo.co.id>
2016-09-07 16:19:33 +0700

    Update filterhistory.php

--- filterhistory.php
+++ filterhistory.php
@@ -1,29 +1,12 @@
 <?php
 
-if (isset($_SERVER['HTTP_ORIGIN'])) {
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-    header('Access-Control-Allow-Credentials: true');
-    header('Access-Control-Max-Age: 86400');    // cache for 1 day
-}
-
-// Access-Control headers are received during OPTIONS requests
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-        header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-
-    exit(0);
-}
-
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json; charset=UTF-8');
+header("Access-Control-Allow-Headers:origin,X-Request-With,Content-Type,Accept");
 
 $link = new mysqli("localhost", "root", "", "jokedb");
 
-$postdata = file_get_contents('php://input');
-$request = json_decode($postdata);
-$uname = mysql_real_escape_string($request->uname);
+$uname = $_GET['person'];
 
 $result = $link->query("SELECT A.id,A.tanggal,A.photo1,A.deskripsi,A.pengirim,A.location,A.lat,A.lng,A.like,B.dp from timelines A ,users B where A.pengirim ='$uname'  AND B.email = '$uname' ORDER BY A.id DESC");
 
@@ -52,5 +35,6 @@ while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
     $outp .= '"dp":"'                       . $rs["dp"]                       . '"}';
 }
 $outp ='{"records":['.$outp.']}';
+$link->close();
 echo($outp);
 ?>
