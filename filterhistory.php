fb0db3ffbe4cb758586f507b6d13e1458b60d6df
redhaanggara <redhabayuanggara@yahoo.co.id>
2016-09-07 17:01:27 +0700

    Update filterhistory.php

--- filterhistory.php
+++ filterhistory.php
@@ -21,7 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-$link = new mysqli("localhost", "root", "", "jokedb");
+$link = new mysqli("us-cdbr-azure-west-b.cleardb.com", "be826d4ad86399", "8670b078", "dbpalhub");
 
 $uname = $_GET['person'];
 $sql = "SELECT A.id,A.tanggal,A.photo1,A.deskripsi,A.pengirim,A.location,A.lat,A.lng,A.like,B.dp from timelines A ,users B where A.pengirim ='$uname'  AND B.email = '$uname' ORDER BY A.id DESC";
