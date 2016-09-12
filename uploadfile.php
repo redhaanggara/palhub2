ed2f3ee544b2fff53de7b1e3d6925abd968a72e6
redhaanggara <redhabayuanggara@yahoo.co.id>
2016-09-12 10:41:56 +0700

    Update uploadfile.php

--- uploadfile.php
+++ uploadfile.php
@@ -19,23 +19,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     exit(0);
 }
 
-
-
+// Create connection
+$mysqli=new mysqli("us-cdbr-azure-west-b.cleardb.com","be826d4ad86399","8670b078", "dbpalhub");
 $postData = file_get_contents('php://input');
 
-$servername = "us-cdbr-azure-west-b.cleardb.com";
-$username = "be826d4ad86399";
-$password = "8670b078";
-$dbname = "dbpalhub";
-
-// Create connection
-$link = mysql_connect($servername, $username, $password);
-// Check connection
-if (!$link) {
-    die("Connection failed: " . mysql_connect_error());
-}
-
-mysql_select_db($dbname, $link);
 $deskripsi = $_POST['deskripsi'];
 $uname = $_POST['uname'];
 $lokasi = $_POST['lokasi'];
@@ -44,37 +31,27 @@ $y=$_POST['lng'];
 print_r($_FILES);
 
 if(!empty($_FILES)){
-
-  //file1
-  $targetfile1 = $_FILES['file1']['tmp_name'];
-  $namafile1 = $_FILES['file1']['name'];
-  $uploadPath1= "timeline/$namafile1";
-
-
-  $sql = "INSERT INTO timelines (photo1,deskripsi,pengirim,location,lat,lng) VALUES
+//file1
+$targetfile1 = $_FILES['file1']['tmp_name'];
+$namafile1 = $_FILES['file1']['name'];
+$uploadPath1= "timeline/$namafile1";
+//-file
+$sql = "INSERT INTO timelines (photo1,deskripsi,pengirim,location,lat,lng) VALUES
         ('$namafile1','$deskripsi','$uname','$lokasi','$x','$y')";
-        
-if (mysql_query($sql))
-{
-move_uploaded_file($targetfile1,"$uploadPath1");
-
-echo json_encode(true);
-echo "ja";
-mysql_close($link);
+if (mysqli_query($mysqli,$sql)){
+        move_uploaded_file($targetfile1,"$uploadPath1");
+        echo json_encode(true);
+        echo "ja";
+        exit();
 }
-
 else{
-  echo json_encode(false);
-  echo "ja";
-}
-}
-
+      echo json_encode(false);
+      echo "ja";
+}}
 else{
-  echo json_encode(false);
-  echo "ja";
+      echo json_encode(false);
+      echo "ja";
 }
-
-
 ?>
 
 
