089f4595c3e04977bc65dbf44756da523e017eec
redhaanggara <redhabayuanggara@yahoo.co.id>
2016-09-12 22:21:48 +0700

    Update uploadfile.php

--- uploadfile.php
+++ uploadfile.php
@@ -1,45 +1,26 @@
 <?php
-if (isset($_SERVER['HTTP_ORIGIN'])) {
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-    header('Access-Control-Allow-Credentials: true');
-    header('Access-Control-Max-Age: 86400');    // cache for 1 day
-}
-
-// Access-Control headers are received during OPTIONS requests
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-
-    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-        header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-
-    exit(0);
-}
-
-$postData = file_get_contents('php://input');
 $servername = "us-cdbr-azure-west-b.cleardb.com";
 $username = "be826d4ad86399";
 $password = "8670b078";
 $dbname = "dbpalhub";
 
 // Create connection
-$link = mysql_connect($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if (!$link) {
-    die("Connection failed: " . mysql_connect_error());
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error());
 }
 
-mysql_select_db($dbname, $link);
 $deskripsi = $_POST['deskripsi'];
 $uname = $_POST['uname'];
 $lokasi = $_POST['lokasi'];
 $x=$_POST['lat'];
 $y=$_POST['lng'];
 $namefile ="cecak";
+
   $sql = "INSERT INTO timelines (photo1,deskripsi,pengirim,location,lat,lng) VALUES
         ('$namafile','$deskripsi','$uname','$lokasi','$x','$y')";
-        if (mysql_query($sql)){
+        if ($conn->query($sql) == TRUE){
                 echo json_encode(true);
                 echo "ja";
           }
