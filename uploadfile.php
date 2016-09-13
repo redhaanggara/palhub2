a210e9b4e8c67e24b2e617374597e4c6448496c0
redhaanggara <redhabayuanggara@yahoo.co.id>
2016-09-13 11:44:58 +0700

    Update uploadfile.php

--- uploadfile.php
+++ uploadfile.php
@@ -24,7 +24,7 @@ $postdata = file_get_contents("php://input");
 if (isset($postdata)) {
    $request = json_decode($postdata);
    $caption = $request->deskripsi;
-   $sql = "INSERT INTO timelines (photo1,deskripsi,pengirim,location,lat,lng,like) VALUES ('null',$caption','null','null',0.02,0.02,0)";
+   $sql = "INSERT INTO timelines (photo1,deskripsi,pengirim,location,lat,lng,like) VALUES ('null','$caption','null','null',002,002,0)";
    mysqli_query($mysqli,$sql);
 }
 else{
