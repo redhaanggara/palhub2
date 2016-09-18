24d2199fcc1dc5415dea0ab4a04d832a0af9e9d4
redhaanggara <redhabayuanggara@yahoo.co.id>
2016-09-18 15:12:15 +0700

    Update uploadfile.php

--- uploadfile.php
+++ uploadfile.php
@@ -6,13 +6,10 @@ if (isset($_SERVER['HTTP_ORIGIN'])) {
 }
 // Access-Control headers are received during OPTIONS requests
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-
     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
         header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-
     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
         header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-
     exit(0);
 }
 $server = "us-cdbr-azure-west-b.cleardb.com";
@@ -22,9 +19,11 @@ $database = "dbpalhub";
 // Koneksi dan memilih database di server
 mysql_connect($server,$username,$password) or die("Koneksi gagal");
 mysql_select_db($database) or die("Database tidak bisa dibuka");
-
 $postdata = file_get_contents('php://input');
-if (isset($postdata)){
+if ($postdata == ""){
+    echo "postkosong";
+}
+else {
     echo "masuke";
     $request = json_decode($postdata);
     $deskripsi = $request->deskripsi;
@@ -36,24 +35,22 @@ if (isset($postdata)){
     $target_file = $target_dir . basename($_FILES["file"]["name"]);
     $namafile = basename( $_FILES["file"]["name"]);
     $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-  if(mysql_query("INSERT INTO timelines(photo1,deskripsi,pengirim,location,lat,lng) VALUES
-        ('$namafile','$deskripsi','$uname','$lokasi','$x','$y'")){
-            if(move_uploaded_file($_FILES["file"]["tmp_name"], $target_file){
-            echo json_encode(true);
-            echo "perfect";
-        }
-            else{
-                echo json_encode(false);
-                echo "gagalmovefile";
-            }
-        }
- else{
-      echo json_encode(false);
-      echo "gagalinsert";
- }
-}
-else {
-    echo "postkosong";
 
+      if(mysql_query("INSERT INTO timelines(photo1,deskripsi,pengirim,location,lat,lng) VALUES
+            ('$namafile','$deskripsi','$uname','$lokasi','$x','$y'")){
+                if(move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)){
+                echo json_encode(true);
+                echo "perfect";
+            }
+                else{
+                    echo json_encode(false);
+                    echo "gagalmovefile";
+                }
+            }
+     else{
+          echo json_encode(false);
+          echo "gagalinsert";
+     }
 }
+
 ?>
