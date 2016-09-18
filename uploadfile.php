d22449fbbb64f000bf937b46fa48849f7736b7b2
redhaanggara <redhabayuanggara@yahoo.co.id>
2016-09-18 15:05:29 +0700

    Update

--- uploadfile.php
+++ uploadfile.php
@@ -24,21 +24,24 @@ mysql_connect($server,$username,$password) or die("Koneksi gagal");
 mysql_select_db($database) or die("Database tidak bisa dibuka");
 
 $postdata = file_get_contents('php://input');
-if (isset($postdata)){
-echo "masuk";
-$request = json_decode($postdata);
-$deskripsi = $request->deskripsi;
-$uname = $request->uname;
-$lokasi = $request->lokasi;
-$x= $request->lat;
-$y= $request->lng;
-$target_dir = "xyz/";
-$target_file = $target_dir . basename($_FILES["file"]["name"]);
-$namafile = basename( $_FILES["file"]["name"]);
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+if ($postdata == ""){
+    echo "postkosong";
+}
+else {
+    echo "masuke";
+    $request = json_decode($postdata);
+    $deskripsi = $request->deskripsi;
+    $uname = $request->uname;
+    $lokasi = $request->lokasi;
+    $x= $request->lat;
+    $y= $request->lng;
+    $target_dir = "xyz/";
+    $target_file = $target_dir . basename($_FILES["file"]["name"]);
+    $namafile = basename( $_FILES["file"]["name"]);
+    $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
   if(mysql_query("INSERT INTO timelines(photo1,deskripsi,pengirim,location,lat,lng) VALUES
         ('$namafile','$deskripsi','$uname','$lokasi','$x','$y'")){
-            if(move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)){
+            if(move_uploaded_file($_FILES["file"]["tmp_name"], $target_file){
             echo json_encode(true);
             echo "perfect";
         }
@@ -51,8 +54,5 @@ $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
       echo json_encode(false);
       echo "gagalinsert";
  }
-}
-else {
-     echo "postkosong";
 }
 ?>
